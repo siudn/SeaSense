08bf61b4e53c77738b8e48d96a7da0a9f2ac8f65
Dev Gandhi <devgandhi914@gmail.com>
2023-11-05 04:44:21 -0500

    Added trained models and the corresponding scripts

---
+++ index.py
@@ -0,0 +1,7 @@
+from imageai.Classification.Custom import ClassificationModelTrainer
+
+model_trainer = ClassificationModelTrainer()
+model_trainer.setModelTypeAsResNet50()
+model_trainer.setDataDirectory("./")
+model_trainer.trainModel(num_experiments=100, batch_size=32)
+
